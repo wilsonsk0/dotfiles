8a7c05e6240b603f84bbb5b37af1b962ccf19592
Sam Wilson <swilson0702@gmail.com>
2024-04-17 20:23:52 -0500

    synchronize changes from work

--- .fzf.bash
+++ .fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/sam/.dotfiles/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/sam/.dotfiles/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/$HOME/.fzf/bin"
 fi
 
 eval "$(fzf --bash)"
