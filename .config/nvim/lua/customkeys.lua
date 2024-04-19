144a87502aaea0aef953b4c54336afec9d791038
Sam Wilson <swilson0702@gmail.com>
2024-04-18 23:20:08 -0500

    tmux setup

--- .config/nvim/lua/customkeys.lua
+++ .config/nvim/lua/customkeys.lua
@@ -56,9 +56,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
-
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -68,7 +68,7 @@ require("lazy").setup({
     },
     {
         "mfussenegger/nvim-dap",
-        config = function ()
+        config = function()
             require("dapkeys")
         end,
     },
@@ -150,5 +150,23 @@ require("lazy").setup({
             require("cmake")
         end,
         event = { "BufReadPre *.cpp", "BufNewFile *.cpp", "BufReadPre CMakeLists.txt", "BufNewFile CMakeLists.txt" },
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
     }
 })
