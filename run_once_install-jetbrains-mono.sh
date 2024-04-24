aa8db99d64038f70c32a306eab80bfe543f5a207
Sam Wilson <test@user.com>
2024-04-23 21:49:38 -0500

    fix font installation

--- run_once_install-jetbrains-mono.sh
+++ run_once_install-jetbrains-mono.sh
@@ -4,7 +4,7 @@ cd /tmp
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
 mkdir JetBrainsMono
 tar -xf JetBrainsMono.tar.xz -C JetBrainsMono
-fc-cache JetBrainsMono
+fc-cache -f JetBrainsMono
 
 rm -rf JetBrainsMono/
 rm JetBrainsMono.tar.xz
