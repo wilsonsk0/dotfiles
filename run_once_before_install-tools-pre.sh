41500b764cc39e44fbf551edcbfb3f982b3c4b4f
Sam Wilson <samuel.wilson@bluehalo.com>
2024-04-25 09:41:32 -0500

    fix fzf installation

--- run_once_before_install-tools-pre.sh
+++ run_once_before_install-tools-pre.sh
@@ -5,7 +5,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 echo Installing fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --key-bindings --completion --update-rc --xdg
+~/.fzf/install --key-bindings --completion --no-update-rc --xdg
 
 echo Installing pyenv
 curl https://pyenv.run | bash
