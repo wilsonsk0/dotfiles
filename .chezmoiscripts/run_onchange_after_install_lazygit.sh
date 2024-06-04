12e2518e25554885b0033f9189c1e3c6cc42f9a2
Sam Wilson <samuel.wilson@bluehalo.com>
2024-06-04 16:05:58 -0500

    improve environment management and move more stuff under .local

---
+++ .chezmoiscripts/run_onchange_after_install_lazygit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go install github.com/jesseduffield/lazygit@latest
+
--- .chezmoiscripts/run_onchange_before_install_rustup.sh
+++ .chezmoiscripts/run_onchange_before_install_rustup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+export RUSTUP_HOME=$HOME/.local/share/rustup
+export CARGO_HOME=$HOME/.local/share/cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
