78ae8f396242205184879c42989c6f4898bfac10
Sam Wilson <samuel.wilson@bluehalo.com>
2024-06-06 16:09:50 -0500

    add lazygit config and fix some other minor issues

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.o.tabstop = 4
 vim.o.softabstop = -1
 vim.o.shiftwidth = 0
 vim.o.expandtab = true
+vim.o.number = true
 vim.g.vsnip_snippet_dir = vim.fn.stdpath("data") .. "/snippets"
 
 -- setup folke/lazy.nvim
