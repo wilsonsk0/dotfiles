c8d7ed6784e8b6ead4975a33f9c600f8e5ed05c2
Sam Wilson <samuel.wilson@bluehalo.com>
2024-09-18 11:47:51 -0500

    modify clangd options and add none-ls

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -3,7 +3,15 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "clangd", "lua_ls", "cmake", "pylsp", "rust_analyzer" },
+                ensure_installed = {
+                    "clangd",
+                    "lua_ls",
+                    "cmake",
+                    "pylsp",
+                    "rust_analyzer",
+                    "glslls",
+                },
+                automatic_installation = true,
             })
         end,
     },
@@ -13,7 +21,13 @@ return {
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            lspconfig.clangd.setup { capabilities = capabilities }
+            lspconfig.clangd.setup {
+                capabilities = capabilities,
+                cmd = {
+                    "clangd",
+                    "--offset-encoding=utf-16",
+                }
+            }
             lspconfig.lua_ls.setup { capabilities = capabilities }
             lspconfig.cmake.setup { capabilities = capabilities }
             lspconfig.pylsp.setup {
@@ -33,6 +47,21 @@ return {
                 }
             }
             lspconfig.rust_analyzer.setup { capabilities = capabilities }
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.code_actions.gitrebase,
+                    null_ls.builtins.diagnostics.cmake_lint,
+                    null_ls.builtins.formatting.cmake_format,
+                    null_ls.builtins.diagnostics.codespell,
+                }
+            })
         end,
     },
 }
